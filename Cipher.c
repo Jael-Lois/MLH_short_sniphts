25c69a7ab26d0bbf826817f7af084994616c1448
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2022-01-13 15:40:20 +0530

    Update Cipher.c

--- Cipher.c
+++ Cipher.c
@@ -2,30 +2,64 @@
 #include<string.h>
 int main()
 {
+  //Encryption
+
+  printf("\nENCRYPTION");
+
   char password[100],encrypt[100];
 
   int l,i,key;
+
   printf("\n\n****************************************************************");
-  printf("\n\nEnter the KEY(10) : ");
+  printf("\n\nEnter the KEY: ");
   scanf("%d",&key);
 
-  printf("Enter the password you want to encrypt : ");
+  printf("\nEnter the message you want to encrypt : ");
   scanf("\n");
   gets(password);
 
   l = strlen(password);
 
   for(i=0;i<l;i++)
-  {
      encrypt[i] = password[i]+key;
-     key=key+10
-  }
+
 
   encrypt[i] = '\0';
 
-  printf("The encryped code is : %s",encrypt);
+  printf("\nThe encryped code is : %s",encrypt);
 
   printf("\n\n****************************************************************");
+
+  key = 0;
+
+  //Decryption
+
+  printf("\nDECRYPTION");
+
+  char message[200],decrypt[200];
+
+  int ll,j,space;
+
+  printf("\n\n****************************************************************");
+
+  printf("\nEnter the Key : ");
+  scanf("%d",&key);
+
+  printf("\nEnter the message you wnt to decrypt : ");
+  scanf("%s",message);
+  scanf("\n");
+  gets(message);
+
+  ll = strlen(message);
+
+  for(j=0;j<ll;j++)
+    decrypt[j] = message[j]-key;
+
+    decrypt[j] = '\0';
+
+    printf("\nThe decrypted code is : %s",decrypt);
+
+    printf("\n\n****************************************************************");
 
   return 0;
 
