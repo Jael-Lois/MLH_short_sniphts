71b4ccd3984b29eece1ca131728d30e14af90212
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2022-01-14 15:47:38 +0530

    Update Game.c

--- Game.c
+++ Game.c
@@ -1,195 +1,240 @@
 
 #include<stdio.h>
-#include<time.h>
+#include<stdlib.h>
+#include<unistd.h>
+
 int main()
 {
 
   int a;
-  printf("__________________________________________________________________________");
+
   printf("\nEnter for choice : 1 or 2");
-  printf("\nOrion, its Cat. I need help");
+  printf("\n__________________________________________________________________________");
+  sleep(2);
+  printf("\n\nOrion, its Cat. I need help");sleep(2);
+  printf("\n\nCHOICE");
   printf("\nYou'r typing the wrong number(1)");
-  printf("\nSorry can't help right now(2)");
+  printf("\nSorry can't help right now(2)");sleep(2);
   scanf("%d",&a);
-  a = a%2 + 1;
+  a = a>2 ? a%2 + 1 : a;
   if(a==1)
   {
-    printf("\nOh....");
-    printf("\nSorry");
-    printf("\nMaybe you can help me");
+    printf("\nOh....");sleep(2);
+    sleep(2);
+    printf("\nSorry");sleep(2);
+    sleep(2);
+    printf("\nMaybe you can help me");sleep(2);
+    sleep(2);
   }
   else if(a==2)
   {
     printf("\nI confirm my request I'm in Berlin");
+    sleep(2);
     printf("\nAndrew gave me this number in case of an emergency");
+    sleep(2);
   }
 
   printf("\nIt is a matter of life or death");
-  printf("\nI really need help now");
+  sleep(2);
+  printf("\nI really need help now");sleep(2);
+  printf("\n\nCHOICE");
   printf("\nOk I will help(1)");
   scanf("%d",&a);
-  a = a%2 + 1;
+  a = a>2 ? a%2 + 1 : a;
   if(a==1)
   {
     printf("\nThank you");
+    sleep(2);
     printf("\nIt's going to sound totally crazy....");
+    sleep(2);
     printf("\nI'm in a hotel room");
+    sleep(2);
     printf("\nSomeone is trying to pick the lock");
+    sleep(2);
     printf("\nI put a chair against the door and locked myself in the Bathroom");
+    sleep(2);
     printf("\nI don't know what to do");
+    sleep(2);
   }
-
+  sleep(2);
+  printf("\n\nCHOICE");
   printf("\nHide, Quick(1)");
-  printf("\nIs there a window(2)");
+  printf("\nIs there a window(2)");sleep(2);
   scanf("%d",&a);
-  a = a%2 + 1;
+  a = a>2 ? a%2 + 1 : a;
   if(a==1)
   {
     printf("\nOk....");
-    printf("\nI hide myself in the cupboard");
-    printf("\nI think they are inside");
-    printf("\nThey are comming towards me");
-    printf("\nI think there gonna.................");
+    sleep(2);
+    printf("\nI hide myself in the cupboard");sleep(2);
+    printf("\nI think they are inside");sleep(2);
+    printf("\nThey are comming towards me");sleep(2);
+    printf("\nI think there gonna.................");sleep(4);
 
-    printf("\nSignal Lost");
-    printf("\nSignal Reconnectin");
+    printf("\nSignal Lost");sleep(2);
+    printf("\nSignal Reconnectin");sleep(2);
     printf("\nCat was kidnapped");
     goto Begin;
   }
   else if(a==2)
   {
-    printf("\nYes, but it is kinda high");
-    printf("\nOh.... wait there is a dumpster");
-    printf("\nIt is full");
-    printf("\nWhat should I do now");
+    printf("\nYes, but it is kinda high");sleep(2);
+    printf("\nOh.... wait there is a dumpster");sleep(2);
+    printf("\nIt is full");sleep(2);
+    printf("\nWhat should I do now");sleep(2);
   }
-
+  printf("\n\nCHOICE");
   printf("\nJump(1)");
-  printf("\nCall for help(2)");
+  printf("\nCall for help(2)");sleep(2);
   scanf("%d",&a);
-  a = a%2 + 1;
+  a = a>2 ? a%2 + 1 : a;
   if(a==1)
   {
-    printf("\nOk...........");
-    printf("\nI landed badly and my arm is hurt");
-    printf("\nIt's bleeding and it hurts badly");
-    printf("\nWhat should I do");
+    printf("\nOk...........");sleep(2);
+    printf("\nI landed badly and my arm is hurt");sleep(2);
+    printf("\nIt's bleeding and it hurts badly");sleep(2);
+    printf("\nWhat should I do");sleep(2);
   }
   else if(a==2)
   {
-    printf("\nOk................");
-    printf("\nThey found me");
-    printf("\nI...................");
-    printf("\nSignal Lost");
-    printf("\nSignal Reconnectin");
+    printf("\nOk................");sleep(2);
+    printf("\nThey found me");sleep(2);
+    printf("\nI...................");sleep(2);
+    printf("\nSignal Lost");sleep(2);
+    printf("\nSignal Reconnectin");sleep(2);
     printf("\nCat was shot");
     goto Begin;
   }
-
+  printf("\n\nCHOICE");
   printf("\nTake it out(1)");
-  printf("\nIt's risky if an artery is involved(2)");
+  printf("\nIt's risky if an artery is involved(2)");sleep(2);
   scanf("%d",&a);
-  a = a%2 + 1;
+  a = a>2 ? a%2 + 1 : a;
   if(a==1)
   {
-    printf("\nOk..........");
-    printf("\nOuch!");
-    printf("\nOk but it is not bleeding much as I expected ");
+    printf("\nOk..........");sleep(2);
+    printf("\nOuch!");sleep(2);
+    printf("\nOk but it is not bleeding much as I expected ");sleep(2);
   }
   else if(a==2)
   {
-    printf("\nYea you are right");
-    printf("\nI know a vet nearby and.........");
+    printf("\nYea you are right");sleep(2);
+    printf("\nI know a vet nearby and.........");sleep(2);
   }
 
-  printf("\nWhat!");
-  printf("\nThis whole area is under surveillance");
-  printf("\nMen in black");
-  printf("\n I need to get out of here");
-  printf("\nAny ideas???");
+  printf("\nWhat!");sleep(2);
+  printf("\nThis whole area is under surveillance");sleep(2);
+  printf("\nMen in black");sleep(2);
+  printf("\n I need to get out of here");sleep(2);
+  printf("\nAny ideas???");sleep(2);
+  printf("\n\nCHOICE");
   printf("\nHug the walls(1)");
-  printf("\nRun as fast as you can(2)");
+  printf("\nRun as fast as you can(2)");sleep(2);
   scanf("%d",&a);
-  a = a%2 + 1;
+  a = a>2 ? a%2 + 1 : a;
   if(a==1)
   {
-    printf("\nSounds reasonable");
-    printf("\nI will text you once I am out");
+    printf("\nSounds reasonable");sleep(2);
+    printf("\nI will text you once I am out");sleep(2);
   }
   else if(a==2)
-  printf("\nHope I did not skip those gym lessons");
+  printf("\nHope I did not skip those gym lessons");sleep(2);
 
   printf("\nCat is busy......");
+  sleep(60);
 
-  printf("\nThey are after me");
-  printf("\nI just hid in a narrow alley");
-  printf("\nI am out of breath");
+  printf("\nThey are after me");sleep(2);
+  printf("\nI just hid in a narrow alley");sleep(2);
+  printf("\nI am out of breath");sleep(2);
+  printf("\n\nCHOICE");
   printf("\nStay hidden(1)");
   printf("\nWhy are they chasing you?(2)");
   scanf("%d",&a);
-  a = a%2 + 1;
+  a = a>2 ? a%2 + 1 : a;
   if(a==1)
-  printf("Ok....");
+  {printf("Ok....");
+  sleep(2);}
   else if(a==2)
-  printf("I will tell you everything later when I get out of here");
+  printf("I will tell you everything later when I get out of here");sleep(2);
 
-  printf("\nThey are coming toward me...........");
-  printf("\nThey caught up with me!");
-  printf("\nI am trapped inside a trunk of a car");
-  printf("\nGood thing I hid my phone just in time");
-  printf("\nThere's got to be a way out of this car");
+  printf("\nThey are coming toward me...........");sleep(20);
+  printf("\nThey caught up with me!");sleep(2);
+  printf("\nI am trapped inside a trunk of a car");sleep(2);
+  printf("\nGood thing I hid my phone just in time");sleep(2);
+  printf("\nThere's got to be a way out of this car");sleep(2);
+  printf("\n\nCHOICE");
   printf("\nLook for a trunk release(1)");
   printf("\nTry to kick the trunk open(2)");
   scanf("%d",&a);
-  a = a%2 + 1;
+  a = a>2 ? a%2 + 1 : a;
   if(a==1)
   {
-    printf("\nI can't find anything");
-    printf("\nOnly thing I found is a screwdriver, a wrench and a wire cutter");
+    printf("\nI can't find anything");sleep(2);
+    printf("\nOnly thing I found is a screwdriver, a wrench and a wire cutter");sleep(2);
   }
   else if(a==2)
   {
-    printf("\nI can't");
-    printf("\nMaybe if I kick harder");
-    printf("\nIt worked");
-    printf("\nOh n......................");
-    printf("\nSignal Lost");
-    printf("\nSignal Reconnectin");
+    printf("\nI can't");sleep(2);
+    printf("\nMaybe if I kick harder");sleep(2);
+    printf("\nIt worked");sleep(2);
+    printf("\nOh n......................");sleep(3);
+    printf("\nSignal Lost");sleep(2);
+    printf("\nSignal Reconnectin");sleep(2);
     printf("\nCat was shot");
     goto Begin;
   }
-
+  printf("\n\nCHOICE");
   printf("\nTry to pick the lock(1)");
   printf("\nBreak the lock by using the wrench(2)");
 
-  printf("\nOk... the car is slowing down");
-  printf("\nGo!");
+  printf("\nOk... the car is slowing down");sleep(2);
+  printf("\nGo!");sleep(2);
+
+  printf("\nCat is busy");sleep(60);
+
+  printf("\nI am out");sleep(2);
+  printf("\nThanks for the help");sleep(2);
+
+  printf("\nI am in the vet");sleep(2);
+  printf("\nI will contact you again when I am fixed up");sleep(2);
 
   printf("\nCat is busy");
+  sleep(60);
 
-  printf("\nI am out");
-  printf("\nThanks for the help");
+  printf("\nIt cost me a few stiches but nothing serious\n");sleep(2);
+  printf("Well\n");
+  printf(" I think I owe you some explanation\n");sleep(2);
+  printf("I guess all this must look preety shady to you\n");sleep(2);
+  printf("I am gonna tell uou what happened to me aver the past few days");sleep(2);
 
-  printf("\nI am in the vet");
-  printf("\nI will contact you again when I am fixed up");
+  printf("\nAlthough I do not clearly understand all that is going on myself \n");sleep(2);
+  printf("Anyway\n");sleep(2);
+  printf("Here are the facts\n");sleep(2);
+  printf(" i am a journalish for a cultural magazine\n");sleep(2);
+  printf("I recived a mysterious letter a week ago \n");sleep(2);
+  printf("It only contained a USB stick with wedding pictures writen on it \n");sleep(2);
+  printf("But when I looked into it there was no pictures\n");sleep(2);
+  printf("It was full of work documents");sleep(2);
 
-  printf("\nCat is busy");
+  printf("\nAt the time\n");sleep(2);
+  printf(" I thought it was just a mistake");sleep(2);
+  printf("\n Then\n");sleep(2);
+  printf("Three days ago\n");sleep(2);
+  printf(" I recived a weird ema;zil from Andrew(childhood friend)");sleep(2);
 
-  printf("\nIt cost me a few stiches but nothing serious\nWell\n I think I owe you some explanation\nI guess all this must look preety shady to you\nI am gonna tell uou what happened to me aver the past few days");
-  printf("\nAlthough I do not clearly understand all that is going on myself \nAnyway\nHere are the facts\n i am a journalish for a cultural magazine\nI recived a mysterious letter a week ago \nIt only contained a USB stick with wedding pictures writen on it \nBut when I looked into it there was no pictures\nIt was full of work documents");
-  printf("\nAt the time\n I thought it was just a mistake\n Then\nThree days ago\n I recived a weird email from Andrew(childhood friend)");
-  printf("\nI was supposed to meet him yesterday But he posponded the meeting");
-  printf("I felt like begin spied");
-  printf("Andrew told me to be careful and bring the wedding pictures with me");
-  printf("\nSo I moved to a hotel");
-  printf("\nAnd changed my phone into a disposable one");
-  printf("\nYou know they can't track you GPS and all");
-  printf("\nHe gave me this number in case of emergency");
-  printf("\nAnd you are the one who answered the phone");
-  printf("\nAnyway thanks again");
+  printf("\nI was supposed to meet him yesterday But he posponded the meeting");sleep(2);
+  printf("I felt like begin spied");sleep(2);
+  printf("Andrew told me to be careful and bring the wedding pictures with me");sleep(2);
+  printf("\nSo I moved to a hotel");sleep(2);
+  printf("\nAnd changed my phone into a disposable one");sleep(2);
+  printf("\nYou know they can't track you GPS and all");sleep(2);
+  printf("\nHe gave me this number in case of emergency");sleep(2);
+  printf("\nAnd you are the one who answered the phone");sleep(2);
+  printf("\nAnyway thanks again");sleep(2);
   printf("\nBye");
 
   Begin:
-
+  printf("\nEnd of the GAME");
+  printf("\n__________________________________________________________________________");
   return 0;
 }
