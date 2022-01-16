cf38f4df42946db90a0a9debd0315a91937f4709
Jael Lois M <81318976+Jael-Lois@users.noreply.github.com>
2022-01-16 12:59:28 +0530

    Create send-Message.c

---
+++ send-Message.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main()
+{
+  sleep(2);
+  printf("\nHello");
+  sleep(2);
+  printf("\nHow are you?");
+  sleep(2);
+  printf("\nI am fine by the grace of God");
+  sleep(2);
+  printf("\nGood to see that you are fine too");
+  sleep(2);
+  printf("\nTodays Bible verse is about");
+  sleep(2);
+  printf("\n\n\nreading the bible each day will help us keep going the right way through the difficult times");
+  sleep(2);
+  printf("\n17  This I say therefore, and testify in the Lord, that ye henceforth walk not as other Gentiles walk, in the vanity of their mind");
+  sleep(2);
+  printf("\n18  Having the understanding darkened, being alienated from the life of God through the ignorance that is in them, because of the blindness of their heart:");
+  sleep(2);
+  printf("\n19  Who being past feeling have given themselves over unto lasciviousness, to work all uncleanness with greediness.");
+  sleep(2);
+  printf("\n20  But ye have not so learned Christ;");
+  sleep(2);
+  printf("\n21  If so be that ye have heard him, and have been taught by him, as the truth is in Jesus:");
+  sleep(2);
+  printf("\n22  That ye put off concerning the former conversation the old man, which is corrupt according to the deceitful lusts;");
+  sleep(2);
+  printf("\n23  And be renewed in the spirit of your mind;");
+  sleep(2);
+  printf("\n24  And that ye put on the new man, which after God is created in righteousness and true holiness.");
+  sleep(2);
+  printf("\n25  Wherefore putting away lying, speak every man truth with his neighbour: for we are members one of another.");
+  sleep(2);
+  printf("\n26  Be ye angry, and sin not: let not the sun go down upon your wrath:");
+  sleep(2);
+  printf("\n27  Neither give place to the devil.");
+  sleep(2);
+  printf("\n28  Let him that stole steal no more: but rather let him labour, working with his hands the thing which is good, that he may have to give to him that needeth.");
+  sleep(2);
+  printf("\n29  Let no corrupt communication proceed out of your mouth, but that which is good to the use of edifying, that it may minister grace unto the hearers.");
+  sleep(2);
+  printf("\n30  And grieve not the holy Spirit of God, whereby ye are sealed unto the day of redemption.");
+  sleep(2);
+  printf("\n31  Let all bitterness, and wrath, and anger, and clamour, and evil speaking, be put away from you, with all malice:");
+  sleep(2);
+  printf("\n32  And be ye kind one to another, tenderhearted, forgiving one another, even as God for Christ's sake hath forgiven you.");
+  sleep(2);
+  return 0;
+}
